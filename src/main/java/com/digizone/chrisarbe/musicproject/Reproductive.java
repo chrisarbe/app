833f18d181b522b1d2a17fff4012b5ee8ed01d26
Christian Arbelaez <cris.arbe@gmail.com>
2019-01-06 19:35:04 -0500

    Reproductor y Lista de reproduccion Copiada

---
+++ src/main/java/com/digizone/chrisarbe/musicproject/Reproductive.java
@@ -0,0 +1,189 @@
+package com.digizone.chrisarbe.musicproject;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.widget.MediaController.*;
+
+
+public class Reproductive extends AppCompatActivity implements MediaPlayerControl {
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    String FilePath, urlAlbum;
+    MediaController mediaController;
+    Handler handler;
+    ImageView imgAlbum;
+    Reproductor variable = new Reproductor();
+    int posRe = 0;//getIntent().getIntExtra("posicion",0);
+    ArrayList<String> lista;
+    public String[] arrPath2;//(String[]) getIntent().getSerializableExtra("arrPath");
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reproductive);
+
+        imgAlbum = (ImageView)findViewById(R.id.imageView_Disco);
+        Bundle bundle = this.getIntent().getExtras();
+        FilePath = bundle.getString("Url");
+        urlAlbum = bundle.getString("urlAlbum");
+        lista = (ArrayList<String>) getIntent().getStringArrayListExtra("arrPath");
+        imgUrlAlbum(urlAlbum);
+        playAudio();
+
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        //startActivity(new Intent(this, Reproductor.class));
+        pause();
+        finish();
+
+    }
+
+    private void imgUrlAlbum (String imgUrlAlbum){
+        if (imgUrlAlbum == null){
+            imgAlbum.setImageResource(R.drawable.play);
+        }else {
+            File imgFile = new File(imgUrlAlbum);
+            if (imgFile.exists()){
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                imgAlbum.setImageBitmap(myBitmap);
+            }
+        }
+    }
+
+    private void playAudio(){
+        mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(Reproductive.this);
+        mediaController.setAnchorView(findViewById(R.id.audioView));
+        handler = new Handler();
+        try {
+            mediaPlayer.setDataSource(FilePath);
+
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(final MediaPlayer mp) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaController.setEnabled(true);
+                        mediaController.show();
+                        mediaPlayer.start();
+                    }
+                });
+            }
+        });
+        /*
+        mediaController.setPrevNextListeners(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("forward button pressed");
+                Object[] objNames = lista.toArray();
+                String[] strNames = Arrays.copyOf(objNames, objNames.length, String[].class);
+                try {
+                    mediaPlayer.setDataSource(strNames[posRe+1]);
+
+                    mediaPlayer.start();
+                    mediaPlayer.prepare();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("backward button pressed");
+                // v is mc
+                // code for previous
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mediaController.show();
+        return true;
+    }
+
+    @Override
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int i) {
+        mediaPlayer.seekTo(i);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        int porcentage = (mediaPlayer.getCurrentPosition()*100)/mediaPlayer.getDuration();
+        return porcentage;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        playAudio();
+    }
+}
--- src/main/java/com/digizone/chrisarbe/musicproject/Reproductor.java
+++ src/main/java/com/digizone/chrisarbe/musicproject/Reproductor.java
@@ -1,17 +1,24 @@
 package com.digizone.chrisarbe.musicproject;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -34,13 +41,34 @@ public class Reproductor extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public static FloatingActionButton home;
-
-    private InterstitialAd mInterstitialAd;
-
     public Reproductor() {
         // Required empty public constructor
     }
+
+    Button play_pause, btn_repetir, btn_anterior,btn_siguiente,btn_stop;
+    MediaPlayer mp;
+    ImageView iv;
+    int repetir = 2, posicion = 0;
+    boolean sonando;
+    int sonandoPos;
+    String[] values = new String[]{};
+    String[] item = new String[]{};
+
+    MediaPlayer vectormp [];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////CAMBIOS REVISAR O ELIMINAR/////////////////////////////////////////////////////////////////////////////
+
+    private int count, i, TRACK_Column, ID_Column, DATA_Column, YEAR_Column, DURATION_Column, ALBUM_ID_Column, ALBUN_Column, ARTIST_Column;
+    private int [] idMusic;
+    TextView title2, artist, time; //pendiente de tiitle2 por solo title.........
+    public String[] audioLista, artistLista, arrPath,musicTime, artistaAlbumLista;
+    ListView lista;
+    public int posGlobal;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////// FIN DE LOS CAMBIOS REVISAR O ELIMINAR/////////////////////////////////////////////////////////////////
 
     /**
      * Use this factory method to create a new instance of
@@ -72,38 +100,267 @@ public class Reproductor extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        final View rootView = inflater.inflate(R.layout.fragment_reproductor, container, false);
 
-        //---------
-        mInterstitialAd = new InterstitialAd(getContext());
-        mInterstitialAd.setAdUnitId("ca-app-pub-8744365861161319/8230498605");
-        //mInterstitialAd.loadAd(new AdRequest.Builder().build());
-        mInterstitialAd.loadAd(new AdRequest.Builder().addTestDevice("DC4FDD8F9668C1895E13BF225BFC8268").build());
+        View rootview = inflater.inflate(R.layout.fragment_reproductor, container, false);
 
-        //---------
+        values = new String[]{};
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////CAMBIOS REVISAR O ELIMINAR/////////////////////////////////////////////////////////////////////////////
 
-        home = (FloatingActionButton) rootView.findViewById(R.id.fab2);
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
-                Fragment fragment = null;
-                fragment = new Home();
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.Contenedor, fragment).commit();
-            }
-        });
-        return rootView;
+        lista = (ListView) rootview.findViewById(R.id.listView_Lista);
+        audioCursor();
+        AudioAdapter audioAdapter = new AudioAdapter();
+        lista.setAdapter(audioAdapter);
+        return rootview;
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////CAMBIOS REVISAR O ELIMINAR/////////////////////////////////////////////////////////////////////////////
+
+    private void audioCursor(){
+        String [] information ={
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.TRACK,
+                MediaStore.Audio.Media.YEAR,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.ALBUM_KEY,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.TITLE_KEY,
+                MediaStore.Audio.Media.ARTIST_ID,
+                MediaStore.Audio.Media.ARTIST
+        };
+
+        final String orderBay = MediaStore.Audio.Media._ID;
+        Cursor audioCursor = getContext().getContentResolver().query(
+                //EXTERNAL_CONTENT_URI para el volumen  de almacenamiento
+                //"primaria" externo.
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, information, null, null, orderBay);
+        count = audioCursor.getCount();
+        audioLista = new String[count];
+        artistLista = new String[count];
+        arrPath = new String[count];
+        musicTime = new String[count];
+        artistaAlbumLista = new String[count];
+
+        ID_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+        DATA_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+        YEAR_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.YEAR);
+        DURATION_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+        ALBUM_ID_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID);
+        ALBUN_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+        TRACK_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+        ARTIST_Column = audioCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+        while (audioCursor.moveToNext()){
+            audioLista[i] = audioCursor.getString(TRACK_Column);
+            artistLista[i] = audioCursor.getString(ARTIST_Column);
+            arrPath[i] = audioCursor.getString(DATA_Column);
+            musicTime[i] = audioCursor.getString(DURATION_Column);
+            artistaAlbumLista[i] = audioCursor.getString(ALBUM_ID_Column);
+            i++;
+        }
+
+        audioCursor.close();
+    }
+
+
+
+    public class AudioAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        public AudioAdapter(){
+            inflater = (LayoutInflater) getContext().getSystemService(
+                    //utilizar el getSystemService(String) para recuperar un LayoutInflater para inflar los recursosde diseño en este contexto.
+                    Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(final int i, View view, ViewGroup viewGroup) {
+            view = inflater.inflate(R.layout.rows,null);
+            title2 = (TextView) view.findViewById(R.id.textView_Title);
+            artist = (TextView) view.findViewById(R.id.textView_Artist);
+            time = (TextView) view.findViewById(R.id.textView_Time);
+
+
+            title2.setId(i);
+            artist.setId(i);
+
+            title2.setText(audioLista[i]);
+            artist.setText(artistLista[i]);
+            long tmp = Integer.parseInt(musicTime[i]);
+            time.setText(convertDuration(tmp));
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                    posGlobal = pos;
+                    String url = arrPath[pos];
+                    String artitsAlbum = artistaAlbumLista[pos];
+                    String urlAlbum = urlAlbunArt(artitsAlbum);
+                    /*Intent intent = new Intent(String.valueOf(Reproductive.class));
+                    intent.putExtra("Url",url);
+                    startActivity(intent);*/
+                    Intent intent = (new Intent(getActivity(), Reproductive.class));
+                    intent.putExtra("Url",url);
+                    intent.putExtra("urlAlbum",urlAlbum);
+                    intent.putExtra("arrPath",arrPath);
+                    intent.putExtra("posicion",pos);
+                    startActivity(intent);
+
+                }
+            });
+
+            return view;
+        }
+
+        public String convertDuration(long duration){
+            String out = null;
+            long hours = 0;
+            try{
+                hours = (duration/3600000);
+            }catch (Exception e){
+                e.printStackTrace();
+                return out;
+            }
+
+            long remaining_minutes =(duration - (hours * 3600000))/60000;
+            String minutes = String.valueOf(remaining_minutes);
+            if(minutes.equals(0)){
+                minutes = "00";
+            }
+            long remaining_seconds = (duration - (hours * 3600000) - (remaining_minutes * 60000));
+            String seconds = String.valueOf(remaining_seconds);
+            if(seconds.length() < 2){
+                seconds = "00";
+            }else{
+                seconds = seconds.substring(0,2);
+            }
+            if(hours > 0){
+                out = hours + ":" + minutes + ":" + seconds;
+            }else{
+                out = minutes + ":" + seconds;
+            }
+            return out;
+        }
+
+        private String urlAlbunArt(String artistAlbum){
+            String[] projection = new  String[]{MediaStore.Audio.Albums.ALBUM_ART};
+            String selection = MediaStore.Audio.Albums._ID + "=?";
+            String[] selectionArt = new String[]{artistAlbum};
+            Cursor cursor = getActivity().getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,projection,selection,selectionArt,null);
+            String urlAlbum = "";
+            if(cursor != null){
+                if (cursor.moveToFirst()){
+                    urlAlbum = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_ART));
+                }
+                cursor.close();
+            }
+            return urlAlbum;
+        }
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////FIN DE LOS CAMBIOS REVISAR O ELIMINAR/////////////////////////////////////////////////////////////////////////////
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    public void PlayPause(View view, int pos){
+        if(vectormp [pos].isPlaying()){
+            vectormp[pos].pause();
+            play_pause.setBackgroundResource(R.drawable.play_circle);
+            //Toast.makeText(getContext(),"Pause",Toast.LENGTH_SHORT).show();
+        }else {
+            try {
+                //vectormp[pos].
+                vectormp[pos].prepareAsync();
+                vectormp[pos].prepare();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), e.toString(), Toast.LENGTH_LONG).show();
+            }
+            vectormp[pos].start();
+            play_pause.setBackgroundResource(R.drawable.stop);
+            //Toast.makeText(getContext(),"Play",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Stop(View view, int pos){
+        if(vectormp[pos] != null){
+            vectormp[pos].stop();
+
+            play_pause.setBackgroundResource(R.drawable.play_circle);
+            //iv.setImageResource(R.drawable.ic_looks_one_black_48dp);
+            //Toast.makeText(getContext(),"Stop",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Repetir(View view){
+        if(repetir == 1){
+            btn_repetir.setBackgroundResource(R.drawable.ic_menu_gallery);
+            Toast.makeText(getContext(),"No Repetir",Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(false);
+            repetir = 2;
+        }else {
+            btn_repetir.setBackgroundResource(R.drawable.ic_menu_camera);
+            Toast.makeText(getContext(),"Repetir",Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(true);
+            repetir = 1;
+        }
+    }
+
+    public void Siguiente (View view){
+
+        if(posicion < vectormp.length -1){
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+
+            }else{
+                posicion++;
+            }
+        }else {
+            Toast.makeText(getContext(),"No Hay Mas Canciones",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Anterior (View view){
+        if(posicion >= 1){
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+
+                posicion--;
+
+                vectormp[posicion].start();
+
+            }else{
+                posicion--;
+            }
+        }else{
+            Toast.makeText(getContext(),"No Hay Más Canciones",Toast.LENGTH_SHORT).show();
         }
     }
 
